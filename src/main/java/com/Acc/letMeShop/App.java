8cd782a56dfc1363d9ed0097b6b8743a6447dd4e
yogi771 <jawaleyogiraj771@gmail.com>
2025-03-07 20:36:48 +0530

    if git is authorised to push

--- src/main/java/com/Acc/letMeShop/App.java
+++ src/main/java/com/Acc/letMeShop/App.java
@@ -4,6 +4,14 @@ public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     
+    int[] arr1 = {20,30,40,60,80};
+    int largest =0;
+    for(int i=0;i<=arr1.length-1;i++)
+    {
+    	//test the git authorization
+    }
+    
+    System.out.println(largest);
     //   https://rahulshettyacademy.com/AutomationPractice/
  //   https://rahulshettyacademy.com/seleniumPractise/#/offers
   // https://rahulshettyacademy.com/seleniumPractise/#/
---
+++ src/main/java/com/Acc/letMeShop/TestDemo.java
@@ -0,0 +1,12 @@
+package com.Acc.letMeShop;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(10+20+"hello"+10+20);
+		
+	}
+
+}
